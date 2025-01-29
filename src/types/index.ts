720129de595fb7dd942b68be33ddde9aacc7df2c
aryan <agarwalaryan139@gmail.com>
2025-01-29 12:04:52 +0530

    wip: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -169,27 +169,6 @@ export interface PythPriceItem {
   ];
 }
 
-export interface OrderParams {
-  quantity: number;
-  side: string;
-  price: number;
-}
-
-export interface BatchOrderPattern {
-  side: string;
-  totalQuantity?: number;
-  priceRange?: {
-    min?: number;
-    max?: number;
-  };
-  spacing?: {
-    type: "percentage" | "fixed";
-    value: number;
-  };
-  numberOfOrders?: number;
-  individualQuantity?: number;
-}
-
 export interface HeliusWebhookResponse {
   webhookURL: string;
   webhookID: string;
