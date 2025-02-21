71e1c2d23d75a52cc7dabf3e6e6b86e2a7cfd8a0
michaelessiet <emsaa2002@gmail.com>
2025-02-21 18:05:00 +0100

    chore: export remaining tools and actions in misc plugin

--- packages/plugin-misc/src/index.ts
+++ packages/plugin-misc/src/index.ts
@@ -1,19 +1,48 @@
 import { Plugin, SolanaAgentKit } from "solana-agent-kit";
 
 // Import all actions
+// alldomains
 import getAllDomainsTLDsAction from "./alldomains/actions/getAllDomainsTLDs";
 import getOwnedAllDomainsAction from "./alldomains/actions/getOwnedAllDomains";
 import getOwnedDomainsForTLDAction from "./alldomains/actions/getOwnedDomainsForTLD";
 import resolveDomainAction from "./alldomains/actions/resolveDomain";
+
+// allora
 import getAllTopicsAction from "./allora/actions/getAllTopics";
 import getInferenceByTopicIdAction from "./allora/actions/getInferenceByTopicId";
 import getPriceInferenceAction from "./allora/actions/getPriceInference";
+
+// gibwork
 import createGibworkTaskAction from "./gibwork/actions/createGibworkTask";
+
+// helius
 import createWebhookAction from "./helius/actions/createWebhook";
 import deleteWebhookAction from "./helius/actions/deleteWebhook";
 import getAssetsByOwnerAction from "./helius/actions/getAssetsbyOwner";
 import getWebhookAction from "./helius/actions/getWebhook";
 import parseSolanaTransactionAction from "./helius/actions/parseTransaction";
+
+// sns
+import resolveSolDomainAction from "./sns/actions/resolveSolDomain";
+import registerDomainAction from "./sns/actions/registerDomain";
+import getPrimaryDomainAction from "./sns/actions/getPrimaryDomain";
+import getMainAllDomainsDomainAction from "./sns/actions/getMainAllDomainsDomain";
+import getAllRegisteredAllDomainsAction from "./sns/actions/getAllRegisteredAllDomains";
+
+// squads
+import transferFromMultisigTreasuryAction from "./squads/actions/transferFromMultisigTreasury";
+import rejectMultisigProposalAction from "./squads/actions/rejectMultisigProposal";
+import executeMultisigProposalAction from "./squads/actions/executeMultisigProposal";
+import depositToMultisigTreasuryAction from "./squads/actions/depositToMultisigTreasury";
+import createMultisigAction from "./squads/actions/createMultisig";
+import createMultisigProposalAction from "./squads/actions/createMultisigProposal";
+import approveMultisigProposalAction from "./squads/actions/approveMultisigProposal";
+
+// switchboard
+import simulateFeedAction from "./switchboard/actions/simulateFeed";
+
+// tiplink
+import createTiplinkAction from "./tiplink/actions/createTiplinks";
 
 // Import all tools
 import {
@@ -34,7 +63,26 @@ import {
   getAssetsByOwner,
   getHeliusWebhook,
   parseTransaction,
+  sendTransactionWithPriorityFee,
 } from "./helius/tools";
+import {
+  resolveSolDomain,
+  registerDomain,
+  getAllRegisteredAllDomains,
+  getMainAllDomainsDomain,
+  getPrimaryDomain,
+} from "./sns/tools";
+import {
+  create_squads_multisig,
+  multisig_create_proposal,
+  multisig_approve_proposal,
+  multisig_deposit_to_treasury,
+  multisig_execute_proposal,
+  multisig_reject_proposal,
+  multisig_transfer_from_treasury,
+} from "./squads/tools";
+import { simulate_switchboard_feed } from "./switchboard/tools";
+import { create_TipLink } from "./tiplink/tools";
 
 // Define and export the plugin
 const MiscPlugin = {
@@ -52,9 +100,24 @@ const MiscPlugin = {
     createGibworkTask,
     create_HeliusWebhook,
     deleteHeliusWebhook,
+    sendTransactionWithPriorityFee,
     getAssetsByOwner,
     getHeliusWebhook,
     parseTransaction,
+    resolveSolDomain,
+    registerDomain,
+    getAllRegisteredAllDomains,
+    getMainAllDomainsDomain,
+    getPrimaryDomain,
+    create_squads_multisig,
+    multisig_create_proposal,
+    multisig_approve_proposal,
+    multisig_deposit_to_treasury,
+    multisig_execute_proposal,
+    multisig_reject_proposal,
+    multisig_transfer_from_treasury,
+    simulate_switchboard_feed,
+    create_TipLink,
   },
 
   // Combine all actions
@@ -72,6 +135,20 @@ const MiscPlugin = {
     getAssetsByOwnerAction,
     getWebhookAction,
     parseSolanaTransactionAction,
+    resolveSolDomainAction,
+    registerDomainAction,
+    getPrimaryDomainAction,
+    getMainAllDomainsDomainAction,
+    getAllRegisteredAllDomainsAction,
+    transferFromMultisigTreasuryAction,
+    rejectMultisigProposalAction,
+    executeMultisigProposalAction,
+    depositToMultisigTreasuryAction,
+    createMultisigAction,
+    createMultisigProposalAction,
+    approveMultisigProposalAction,
+    simulateFeedAction,
+    createTiplinkAction,
   ],
 
   // Initialize function
